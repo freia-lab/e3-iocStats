a5ef983a898e887d3789d2fe9dd7996c0e34e447
Marek Jacewicz <marek.jacewicz@physics.uu.se>
2025-11-06 17:21:46 +0100

    Now iocGeneralTime.template is also in freia patch

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,3 +1,8 @@
 # This should be a test or example startup script
 
-require iocStats
+epicsEnvSet ("IOCNAME", "MJioc-test")
+epicsEnvSet("ENGINEER", "MJ")
+
+require iocstats
+
+dbLoadRecords("iocAdminSoft-freia.db", "IOC=$(IOCNAME)")
